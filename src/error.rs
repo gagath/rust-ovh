7d040b7d8afbca465b1e0c7f5821b3c8141a4ffb
Aedius <aedius.filmania@gmail.com>
2023-07-31 20:41:17 +0200

    derive error from previous

--- src/error.rs
+++ src/error.rs
@@ -1,23 +1,22 @@
+use reqwest::Error as ReqError;
+use serde_json::Error as SerError;
+use std::num::{ParseIntError, TryFromIntError};
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum OvhError {
-
     #[error("network issue")]
-    Reqwest,
+    Reqwest(#[from] ReqError),
 
     #[error("parseInt issue")]
-    ParseIntError,
+    ParseIntError(#[from] ParseIntError),
 
     #[error("tryFromInt issue")]
-    TryFromInt,
+    TryFromInt(#[from] TryFromIntError),
 
     #[error("serde issue")]
-    Serde,
+    Serde(#[from] SerError),
 
     #[error("generic error : `{0}`")]
     Generic(String),
-
-    #[error("unknown data store error")]
-    Unknown,
 }
